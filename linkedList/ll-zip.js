bbedc6dd743f1c2d120ea9df4ebd13c7f30cfbb9
AhmadHirthani <hertani86@gmail.com>
2020-09-16 08:20:36 +0300

    finishLlzip

---
+++ linkedList/ll-zip.js
@@ -0,0 +1,24 @@
+
+const LinkedList = require('./linked-list.js');
+
+function zipLists(list1, list2) {
+    const newList = new LinkedList();
+  
+    let current1 = list1.head;
+    let current2 = list2.head;
+  
+    while (current1 || current2) {
+      if (current1) { 
+        newList.append(current1.value);
+        current1 = current1.next;
+      }
+  
+      if (current2) {  
+        newList.append(current2.value);
+        current2 = current2.next;
+      }
+    }
+    return newList;
+  }
+
+  module.exports = zipLists;
