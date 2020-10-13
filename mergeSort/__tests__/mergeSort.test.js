63a6cd36af3d747b52051f82e72a2993b52abead
AhmadHirthani <hertani86@gmail.com>
2020-10-13 07:34:39 +0300

    finishMergeSort

--- mergeSort/__tests__/mergeSort.test.js
+++ mergeSort/__tests__/mergeSort.test.js
@@ -1,28 +1,21 @@
 'use strict';
 
-const insertionSort = require('../insertionSort.js');
+const mergeSort = require('../mergeSort.js');
 
-describe('insertion sort module', () => {
+describe('Merge sort module', () => {
   it('Test normal array', () => {
     let arr = [7, 8, 5, 2, 4, 6, 3];
     let expected = [2, 3, 4, 5, 6, 7, 8];
-    expect(insertionSort(arr)).toEqual(expected);
-
+    expect(mergeSort(arr)).toEqual(expected);
   }),
   it('Test nearly sorted array', () => {
     let nearlySorted = [2, 3, 5, 7, 13, 11];
     let nearlySortedExpected = [2, 3, 5, 7, 11, 13];
-    expect(insertionSort(nearlySorted)).toEqual(nearlySortedExpected);
+    expect(mergeSort(nearlySorted)).toEqual(nearlySortedExpected);
   }),
   it('Test few uniques array', () => {
     let fewUniques = [5, 12, 7, 5, 5, 7];
     let fewUniquesExpected = [5, 5, 5, 7, 7, 12];
-    expect(insertionSort(fewUniques)).toEqual(fewUniquesExpected);
-  })
-
-
-
-    [5, 12, 7, 5, 5, 7]
-
-
+    expect(mergeSort(fewUniques)).toEqual(fewUniquesExpected);
+  });
 })
